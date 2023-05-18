e1655450679a47fef0f7230b8167039e26413376
Mahadi90 <mahadimhs787@gmail.com>
2023-05-18 11:23:55 +0600

    get gallery added

--- index.js
+++ index.js
@@ -29,8 +29,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const galleryCollection = client.db('carsToyDB').collection('gallery');
 
-    
+
+    app.get('/gallery', async(req, res) => {
+      const result = await galleryCollection.find().toArray();
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
