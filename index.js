9b3c8edeccc98a77b6adc0c1caeb575a02ebdce7
Mahadi90 <mahadimhs787@gmail.com>
2023-05-19 11:25:19 +0600

    delted toys added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -65,15 +65,29 @@ async function run() {
 
     app.get('/myToys/:email', async(req, res)=>{
       const email = req.params.email;
-      console.log(email)
+      // console.log(email)
       const result = await carsCollecttion.find({email : email}).toArray();
       res.send(result)
     })
+    
 
     app.post('/cars', async(req, res) => {
       const body = req.body;
       // console.log(body)
       const result = await carsCollecttion.insertOne(body);
+      res.send(result)
+    })
+
+    // app.put('/allToys/:id', async(req, res)=> {
+    //   const id = req.params.id;
+    //   const updatedToy = req.body;
+    //   console.log(id, updatedToy)
+    // })
+
+    app.delete('/allToys/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)}
+      const result = await carsCollecttion.deleteOne(query)
       res.send(result)
     })
 
